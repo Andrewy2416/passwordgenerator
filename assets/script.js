10a7d12f8ccd9a22c156ec71df8d77831da598b4
Andrewy2416 <andrewy2416@gmail.com>
2021-10-06 01:40:17 -0700

    Added array for letters, special characters, and numbers

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,27 @@
+var characterLength = 8;
+var choiceArray = [];
+
+var specialCharArray = ['!','@','#','$','%','^','&','*','(',')','?','~','/','|',':','>','<','"',];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArray = ['1','2','3','4','5','6','7','8','9','0',];
+
+
+
+
+
+
 // Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +32,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
