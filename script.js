2b8b8757dea84e857ebd0b71effa3e1098928a7a
Andrewy2416 <andrewy2416@gmail.com>
2021-10-06 03:08:58 -0700

    added writePassword function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var characterLength = 8;
-var choiceArray = [];
+var choiceArr = [];
 
-var specialCharArray = ['!','@','#','$','%','^','&','*','(',')','?','~','/','|',':','>','<','"',];
-var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numberArray = ['1','2','3','4','5','6','7','8','9','0',];
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','?','~','/','|',':','>','<','"',];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0',];
 
 
 
@@ -26,10 +26,12 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
     var correctPrompts = getPrompts(); 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    if(correctPrompts) {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+
+      passwordText.value = password;
 
 }
 
@@ -41,7 +43,7 @@ function generatePassword() {
 
 
 function getPrompts(){
-  choiceArray = [];
+    choiceArr = [];
     characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
 
     if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
@@ -50,22 +52,22 @@ function getPrompts(){
     }    
 
     if (confirm("Would you like lowercase letters in your password")) {
-      choiceArr = choiceArray.concat(lowerCaseArray);
+      choiceArr = choiceArr.concat(lowerCaseArr);
 
     }
 
     if (confirm("Would you like uppercase letters in your password")) {
-      choiceArr = choiceArray.concat(upperCaseArray);
+      choiceArr = choiceArr.concat(upperCaseArr);
 
     }
 
     if (confirm("Would you like special characters in your password")) {
-      choiceArr = choiceArray.concat(specialCharArray);
+      choiceArr = choiceArr.concat(specialCharArr);
 
     }
 
     if (confirm("Would you like numbers in your password")) {
-      choiceArr = choiceArray.concat(numberArray);
+      choiceArr = choiceArr.concat(numberArr);
 
     }
     return true;
@@ -74,4 +76,4 @@ function getPrompts(){
 
 }
 
-
+}
